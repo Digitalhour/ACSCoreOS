ef913539fd036f058b83816cd2b7af9947065b3c
Digitalhour <chris.aldridge@gmail.com>
2025-07-31 13:51:15 -0400

    Update BlogPublished.php

--- CoreOS/app/Notifications/BlogPublished.php
+++ CoreOS/app/Notifications/BlogPublished.php
@@ -8,7 +8,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Redis;
 
 class BlogPublished extends Notification implements ShouldQueue
 {
@@ -19,8 +18,8 @@ class BlogPublished extends Notification implements ShouldQueue
     public function __construct(BlogArticle $blogArticle)
     {
         $this->blogArticle = $blogArticle;
-        $this->onConnection('redis')
-            ->onQueue('default');
+        // Use default queue connection from config
+        $this->onQueue('default');
 
         Log::info('Blog Published notification constructed', [
             'blog_id' => $blogArticle->id,
@@ -28,17 +27,7 @@ class BlogPublished extends Notification implements ShouldQueue
             'author' => $blogArticle->user->name,
             'time' => now()->toDateTimeString(),
             'queue_connection' => config('queue.default'),
-            'redis_connection' => config('database.redis.default'),
         ]);
-
-        try {
-            Redis::connection()->ping();
-            Log::info('Redis connection successful for blog notification');
-        } catch (\Exception $e) {
-            Log::error('Redis connection failed for blog notification', [
-                'error' => $e->getMessage()
-            ]);
-        }
     }
 
     public function via($notifiable): array
