51a1bea0fd3128152ca05e492d283ba9f5eb134b
Digitalhour <chris.aldridge@gmail.com>
2025-08-12 10:57:18 -0400

    Update app.php

--- CoreOS/bootstrap/app.php
+++ CoreOS/bootstrap/app.php
@@ -3,7 +3,6 @@
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
 use App\Http\Middleware\NavigationDataMiddleware;
-use App\Http\Middleware\RoutePermissionMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -24,7 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
             NavigationDataMiddleware::class,
-            RoutePermissionMiddleware::class,
+//            RoutePermissionMiddleware::class,
         ]);
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
