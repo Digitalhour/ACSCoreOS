93416488ae9f9f9c96e5b01e268798f89da77af3
Digitalhour <chris.aldridge@gmail.com>
2025-08-12 11:05:34 -0400

    Update app.php

--- CoreOS/bootstrap/app.php
+++ CoreOS/bootstrap/app.php
@@ -28,7 +28,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
-            'route.permission' => \App\Http\Middleware\RoutePermissionMiddleware::class,
+//            'route.permission' => \App\Http\Middleware\RoutePermissionMiddleware::class,
         ]);
 
     })
