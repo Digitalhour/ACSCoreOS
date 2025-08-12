3a7a0eb2d617fa6d47bfb2484ccd3fe10b42193f
Digitalhour <chris.aldridge@gmail.com>
2025-08-12 14:36:51 -0400

    Update training.php

--- CoreOS/routes/training.php
+++ CoreOS/routes/training.php
@@ -3,33 +3,28 @@
 // Old Style Training Tracking Routes
 use App\Http\Controllers\OldStyleTrainingTrackingController;
 use Illuminate\Support\Facades\Route;
-use Laravel\WorkOS\Http\Middleware\ValidateSessionWithWorkOS;
 
 
-Route::middleware('auth')
-    ->middleware(ValidateSessionWithWorkOS::class)
-    ->group(function () {
-
-        Route::group([
-            'middleware' => ['auth', 'verified', 'route.permission']
-        ], function () {
-            Route::get('/old-style-training-tracking', [OldStyleTrainingTrackingController::class, 'index'])
+Route::group([
+    'middleware' => ['auth', 'verified', 'route.permission'],
+    'prefix' => 'old-style-training-tracking',
+], function () {
+            Route::get('/', [OldStyleTrainingTrackingController::class, 'index'])
             ->name('old-style-training-tracking.index');
 
-            Route::post('/old-style-training-tracking', [OldStyleTrainingTrackingController::class, 'store'])
+            Route::post('/', [OldStyleTrainingTrackingController::class, 'store'])
             ->name('old-style-training-tracking.store');
 
-            Route::put('/old-style-training-tracking/{type}/{id}', [OldStyleTrainingTrackingController::class, 'update'])
+            Route::put('/{type}/{id}', [OldStyleTrainingTrackingController::class, 'update'])
             ->name('old-style-training-tracking.update');
 
-            Route::delete('/old-style-training-tracking/{type}/{id}', [OldStyleTrainingTrackingController::class, 'destroy'])
+            Route::delete('/{type}/{id}', [OldStyleTrainingTrackingController::class, 'destroy'])
             ->name('old-style-training-tracking.destroy');
 
-            Route::get('/old-style-training-tracking/export-data', [OldStyleTrainingTrackingController::class, 'exportData'])
+            Route::get('/export-data', [OldStyleTrainingTrackingController::class, 'exportData'])
             ->name('old-style-training-tracking.export-data');
 
-            Route::get('/old-style-training-tracking/export-logs', [OldStyleTrainingTrackingController::class, 'exportLogs'])
+            Route::get('/export-logs', [OldStyleTrainingTrackingController::class, 'exportLogs'])
             ->name('old-style-training-tracking.export-logs');
 
-        });
 });
