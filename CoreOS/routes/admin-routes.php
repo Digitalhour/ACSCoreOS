585a6e3b39cb821771e9af742dfdc2bbefef269a
Digitalhour <chris.aldridge@gmail.com>
2025-08-12 09:42:05 -0400

    changes

--- CoreOS/routes/admin-routes.php
+++ CoreOS/routes/admin-routes.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\ActivityLogController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 use Laravel\WorkOS\Http\Middleware\ValidateSessionWithWorkOS;
 
 
@@ -11,16 +10,16 @@ Route::middleware('auth')
     ->group(function () {
 
 
-    Route::get('/admin', function () {
-        return Inertia::render('Admin/adminDashboard', [
-            'title' => 'Admin Dashboard',
+//    Route::get('/admin', function () {
+//        return Inertia::render('Admin/adminDashboard', [
+//            'title' => 'Admin Dashboard',
 //            'userStats' => [
 //                'totalUsers' => User::count(),
 //                'activeUsers' => User::where('last_login_at', '>=', now()->subDays(30))->count(),
 //                'totalLogins' => User::sum('login_count')
 //            ]
-        ]);
-    })->name('admin.index');
+//        ]);
+//    })->name('admin.index');
 
 
     Route::get('dev-ops/activity-log', [ActivityLogController::class, 'index'])->name('activity-log.index');
